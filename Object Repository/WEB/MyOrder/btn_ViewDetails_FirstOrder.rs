0d4510d1b142821bd2cd0ab024e5e9b045d1513a
AgungPriyadi <AgungPriyadi@NB-GDA-55026>
2026-06-24 18:06:04 +0700

    - Add Cancel Order test suite and test case - Add cart add/update/remove product validations - Add cart quantity verification - Add promotion code validation - Add Midtrans payment instruction verification - Add cancel reason copywriting validation - Add checkout cancel order scenario - Add View Details locator for first order - Add View Details locator for latest unpaid order - Add Unpaid tab locator and active state validation - Update Regression_pasti_ada_jalan_Web suite collection

---
+++ Object Repository/WEB/MyOrder/btn_ViewDetails_FirstOrder.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_ViewDetails_FirstOrder</name>
+   <tag></tag>
+   <elementGuidId>d76491da-5304-4139-ae11-f17105b0b195</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//button[.//span[normalize-space()='View Details']])[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/WEB/MyOrder/tab_Unpaid_Active.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>tab_Unpaid_Active</name>
+   <tag></tag>
+   <elementGuidId>43612888-4e81-44fd-bac6-748b7434483b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@role='tab'&#xd;
+      and normalize-space()='Unpaid'&#xd;
+      and contains(@class,'is-active')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
