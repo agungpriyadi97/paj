a004d0a9c79114a889848bf9f679a738219c22eb
Agung Priyadi <agungpriyadi97@gmail.com>
2026-07-09 10:57:39 +0700

    Revert "Revert "Revert "Revert "Revert "Feature"""""

---
+++ Test Suites/Cencel Order.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Cencel Order</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>b5da87e8-2119-4eaf-9c07-8849db5edc32</testSuiteGuid>
+   <testCaseLink>
+      <guid>235b3e0f-1383-4e5c-8caa-3d1bc6304eb9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WEB/Checkout/Positive - Cancel Order</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
